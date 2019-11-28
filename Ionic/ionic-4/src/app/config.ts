4418c5d1d71054acb9031379f7b137b2de2d3860
ronaldeu <rabellano@you-source.com>
2019-11-28 14:50:23 +0800

    google services package name must be the same as the android manifest package name

--- Ionic/ionic-4/src/app/config.ts
+++ Ionic/ionic-4/src/app/config.ts
@@ -1,4 +1,5 @@
 export const config = {
-    hubName: '',
-    hubConnectionString: ''
+    hubName: 'Dev-ZQR-NotificationHub',
+    // tslint:disable-next-line: max-line-length
+    hubConnectionString: 'sb://dev-zqr-notificationhub.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=GI/bMVU89lZsb9bluTveYsuM7mRr3gswJ41ymxtVvFM='
 };
